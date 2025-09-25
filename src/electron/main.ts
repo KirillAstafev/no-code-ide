a14df18877d82cc0896e421427ac20bd03ec2a65
KirillAstafev <gamerstyle.ya.ru@gmail.com>
2025-09-26 00:46:45 +0300

    Стартовый проект

---
+++ src/electron/main.ts
@@ -0,0 +1,9 @@
+import { app, BrowserWindow } from "electron";
+
+app.on("ready", () => {
+    const mainWindow = new BrowserWindow({
+       fullscreen: true
+    });
+
+    mainWindow.loadURL("http://localhost:5123");
+});
