0e18478145f1a138b73c2939af79b77e5a00e0b9
KirillAstafev <gamerstyle.ya.ru@gmail.com>
2025-09-26 11:46:29 +0300

    Обновление .gitignore

--- dist-electron/main.js
+++
@@ -1,7 +0,0 @@
-import { app, BrowserWindow } from "electron";
-app.on("ready", () => {
-    const mainWindow = new BrowserWindow({
-        fullscreen: true
-    });
-    mainWindow.loadURL("http://localhost:5123");
-});
