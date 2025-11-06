5e869c0171854324d7b8b1dbca3ef175d59c2494
KirillAstafev <gamerstyle.ya.ru@gmail.com>
2025-11-06 21:13:58 +0300

    Добавлены стили Gravity UI; изменён стиль body

--- src/react/main.tsx
+++ src/react/main.tsx
@@ -1,9 +1,14 @@
-import { StrictMode } from 'react'
+import '@gravity-ui/uikit/styles/fonts.css';
+import '@gravity-ui/uikit/styles/styles.css';
+
+import './styles.css'
+
 import { createRoot } from 'react-dom/client'
+import { ThemeProvider } from "@gravity-ui/uikit";
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <ThemeProvider theme="system">
     <App />
-  </StrictMode>,
+  </ThemeProvider>,
 )
